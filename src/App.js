3e9be1cb0fcaf5f28ec32f42f0f6109a26cb0ef8
tkoivis18 <tobias.koivisto@gmail.com>
2023-03-16 14:56:56 +0200

    finished app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
+import AddBook from './AddBook';
+
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const [columnDefs] = useState([
+    { field: 'title', suppressMovable: true },
+    { field: 'author', suppressMovable: true },
+    { field: 'year', suppressMovable: true },
+    { field: 'isbn', suppressMovable: true },
+    { field: 'price', suppressMovable: true },
+  ]);
+
+  const fetchItems = () => {
+    fetch(
+      'https://bookstore-e9ea7-default-rtdb.europe-west1.firebasedatabase.app/books/.json'
+    )
+      .then((response) => response.json())
+      .then((data) => addKeys(data))
+      .catch((err) => console.error(err));
+  };
+
+  const addKeys = (data) => {
+    const keys = Object.keys(data);
+    const valueKeys = Object.values(data).map((item, index) =>
+      Object.defineProperty(item, 'id', { value: keys[index] })
+    );
+    setBooks(valueKeys);
+  };
+
+  const addBook = (newBook) => {
+    fetch(
+      'https://bookstore-e9ea7-default-rtdb.europe-west1.firebasedatabase.app/books/.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(newBook),
+      }
+    )
+      .then((response) => fetchItems())
+      .catch((err) => console.error(err));
+  };
+
+  const deleteBook = (id) => {
+    fetch(
+      `https://bookstore-e9ea7-default-rtdb.europe-west1.firebasedatabase.app/books/${id}.json`,
+      {
+        method: 'DELETE',
+      }
+    )
+      .then((response) => fetchItems())
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5">Bookstore</Typography>
+        </Toolbar>
+      </AppBar>
+      <AddBook addBook={addBook} />
+      <div
+        className="ag-theme-material"
+        style={{ height: 400, width: 600, margin: 'auto' }}
+      >
+        <AgGridReact
+          rowData={books}
+          columnDefs={columnDefs}
+          animateRows={true}
+          suppressDragLeaveHidesColumns={true}
+        />
+      </div>
     </div>
   );
 }
